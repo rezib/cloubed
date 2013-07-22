074583696cf34aa8c3293758e38a4c59374d19df
Rémi Palancher <remi@rezib.org>
2013-07-22 22:02:34 +0200

    slight update of setup.py
    Added missing license in header and new metadata.

--- setup.py
+++ setup.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 Rémi Palancher
+#
+# This file is part of Cloubed.
+#
+# Cloubed is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Cloubed is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with Cloubed.  If not, see
+# <http://www.gnu.org/licenses/>.
 
 from distutils.core import setup
 
@@ -9,4 +28,7 @@ setup(name='Cloubed',
       author_email='remi@rezib.org',
       package_dir = {'cloubed': 'lib'},
       packages=['cloubed'],
+      requires=['libvirt', 'yaml'],
+      license="LGPLv3",
+      platforms=['GNU/Linux']
      )
