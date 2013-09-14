68715ee49351c23c89dba71b90405a0ce083f9e7
Rémi Palancher <remi@rezib.org>
2013-09-14 14:40:55 +0200

    moved args management to new CloubedArgumentParser
    New classe CloubedArgumentParser has been added for most logic of cloubed CLI
    script arguments management.

---
+++ lib/cli/CloubedArgumentParser.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013 Rémi Palancher
+#
+# This file is part of Cloubed.
+#
+# Cloubed is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 3 of
+# the License, or (at your option) any later version.
+#
+# Cloubed is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with Cloubed.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+""" Cloubed script arguments parser """
+
+from cloubed.CloubedException import CloubedArgumentException
+import argparse
+import logging
+
+class CloubedArgumentParser(argparse.ArgumentParser):
+
+    def __init__(self, description, **kwargs):
+
+        argparse.ArgumentParser.__init__(self, description, **kwargs)
+
+    def error(self, message):
+
+        #self.print_usage(_sys.stderr)
+        #self.exit(2, _('%s: error: %s\n') % (self.prog, message))
+        raise CloubedArgumentException(message)
+
+    def parse_args(self):
+
+        self._args = super(CloubedArgumentParser, self).parse_args()
+
+        return self._args
+
+    def add_args(self):
+
+        self.add_argument("actions",
+                            metavar="ACTION",
+                            nargs=1,
+                            choices=['gen', 'boot', 'wait'],
+                            help="name of the action to perform")
+
+        # TODO: actually still to be implemented
+        self.add_argument("-c", "--conf",
+                            dest="configuration_filename",
+                            default="cloubed.yaml",
+                            help="Alternate testbed YAML file (default: " \
+                                 "cloubed.yaml)")
+
+        self.add_argument("-d", "--debug",
+                            help='Enable debug output',
+                            action="store_true")
+
+        self.add_argument("--domain",
+                            dest="domain",
+                            nargs=1,
+                            help="The domain on which the action will be performed",
+                            required=True )
+
+        # We create argparse groups of arguments in order to classify parameters by
+        # actions in the output of --help. See documentation of argparse module for
+        # more details about this feature.
+        parser_boot_grp = self.add_argument_group('Arguments for boot action')
+        parser_gen_grp = self.add_argument_group('Arguments for gen action')
+        parser_wait_grp = self.add_argument_group('Arguments for wait action')
+
+        parser_boot_grp.add_argument("--bootdev",
+                            dest='bootdev',
+                            nargs='?',
+                            choices=['hd', 'network', 'cdrom'],
+                            help="Boot device with boot action")
+
+        parser_boot_grp.add_argument("--wait-event",
+                            dest="waited_event",
+                            nargs=1,
+                            help="Wait for an event before returning with boot action",)
+
+        parser_boot_grp.add_argument("--overwrite-disks",
+                            dest='overwrite_disks',
+                            nargs='+',
+                            help="Overwrite storage volume with boot action" \
+                                 " (default: no, possible values are: yes, no or" \
+                                 " a list of disks separated by blank spaces)")
+
+        parser_boot_grp.add_argument("--recreate-networks",
+                            dest='recreate_networks',
+                            nargs='+',
+                            help="Recreate networks with boot action (default: no," \
+                                 " possible values are: yes, no or a list of networks" \
+                                 " separated by blank spaces)")
+
+        parser_gen_grp.add_argument("--filename",
+                            dest='filename',
+                            nargs=1,
+                            help="Template file name to generate")
+
+        parser_wait_grp.add_argument("--event",
+                            dest='event',
+                            nargs=1,
+                            help="Event to wait")
+
+    def check_coherency(self):
+
+        action = self._args.actions[0]
+
+        logging.debug(u"check for incoherent parameters in action {action}" \
+                          .format(action=action))
+
+        error_str = u"{attribute} has no sense with {action} action"
+
+        if action == "boot":
+
+            # for gen
+            if self._args.filename:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--filename",
+                                           action=action))
+            # for wait
+            if self._args.event:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--event",
+                                           action=action))
+
+        elif action == "gen":
+
+            # for boot
+            if self._args.bootdev:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--bootdev",
+                                           action=action))
+            if self._args.waited_event:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--wait-event",
+                                           action=action))
+            if self._args.overwrite_disks:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--overwrite-disks",
+                                           action=action))
+            if self._args.recreate_networks:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--recreate-networks",
+                                           action=action))
+            # for wait
+            if self._args.event:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--event",
+                                           action=action))
+
+        elif action == "wait":
+
+            # for boot
+            if self._args.bootdev:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--bootdev",
+                                           action=action))
+            if self._args.waited_event:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--wait-event",
+                                          action=action))
+            if self._args.overwrite_disks:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--overwrite-disks",
+                                           action=action))
+            if self._args.recreate_networks:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--recreate-networks",
+                                           action=action))
+            # for gen
+            if self._args.filename:
+                raise CloubedArgumentException(
+                          error_str.format(attribute="--filename",
+                                           action=action))
+
+    def check_bootdev(self):
+
+        if self._args.bootdev:
+            return self._args.bootdev
+        else:
+            return 'hd' # default value
+            # It is not set in parameter of argparse.add_argument(default='hd')
+            # because in this case args.bootdev is therefore defined all the
+            # times and it raises errors in check_args_coherency() for
+            # action != boot
--- lib/cli/CloubedCli.py
+++ lib/cli/CloubedCli.py
@@ -22,165 +22,30 @@
 """ All functions for cloubed CLI script"""
 
 from cloubed.Cloubed import Cloubed
-from cloubed.CloubedException import CloubedException
-import argparse
+from cloubed.CloubedException import CloubedException, CloubedArgumentException
+from cloubed.cli.CloubedArgumentParser import CloubedArgumentParser
 import sys
 import logging
 
-# TODO: wait ssh and cleanup
-
-def parse_args():
-    
-    parser = argparse.ArgumentParser(
-                 description=u"Perform action over your virtual testbed" )
-
-    parser.add_argument("actions",
-                        metavar="ACTION",
-                        nargs=1,
-                        choices=['gen', 'boot', 'wait'],
-                        help="name of the action to perform")
-
-    # TODO: actually still to be implemented
-    parser.add_argument("-c", "--conf",
-                        dest="configuration_filename",
-                        default="cloubed.yaml",
-                        help="Alternate testbed YAML file (default: " \
-                             "cloubed.yaml)")
-
-    parser.add_argument("-d", "--debug",
-                        help='Enable debug output',
-                        action="store_true")
-
-    parser.add_argument("--domain",
-                        dest='domain',
-                        nargs=1,
-                        help="The domain on which the action will be performed",
-                        required=True )
-
-    # We create argparse groups of arguments in order to classify parameters by
-    # actions in the output of --help. See documentation of argparse module for
-    # more details about this feature.
-    parser_boot_grp = parser.add_argument_group('Arguments for boot action')
-    parser_gen_grp = parser.add_argument_group('Arguments for gen action')
-    parser_wait_grp = parser.add_argument_group('Arguments for wait action')
-
-    parser_boot_grp.add_argument("--bootdev",
-                        dest='bootdev',
-                        nargs='?',
-                        choices=['hd', 'network', 'cdrom'],
-                        help="Boot device with boot action")
-
-    parser_boot_grp.add_argument("--wait-event",
-                        dest="waited_event",
-                        nargs=1,
-                        help="Wait for an event before returning with boot action",)
-
-    parser_boot_grp.add_argument("--overwrite-disks",
-                        dest='overwrite_disks',
-                        nargs='+',
-                        help="Overwrite storage volume with boot action" \
-                             " (default: no, possible values are: yes, no or" \
-                             " a list of disks separated by blank spaces)")
-
-    parser_boot_grp.add_argument("--recreate-networks",
-                        dest='recreate_networks',
-                        nargs='+',
-                        help="Recreate networks with boot action (default: no," \
-                             " possible values are: yes, no or a list of networks" \
-                             " separated by blank spaces)")
-
-    parser_gen_grp.add_argument("--filename",
-                        dest='filename',
-                        nargs=1,
-                        help="Template file name to generate")
-
-    parser_wait_grp.add_argument("--event",
-                        dest='event',
-                        nargs=1,
-                        help="Event to wait")
-
-    args = parser.parse_args()
-
-    return parser, args
-
-def check_args_coherency(parser, args):
-
-    action = args.actions[0]
-
-    logging.debug(u"check for incoherent parameters in action {action}" \
-                      .format(action=action))
-
-    error_str = u"{attribute} has no sense with {action} action"
-
-    if action == "boot":
-
-        # for gen
-        if args.filename:
-            parser.error(error_str.format(attribute="--filename",
-                                          action=action))
-        # for wait
-        if args.event:
-            parser.error(error_str.format(attribute="--event", action=action))
-
-    elif action == "gen":
-
-        # for boot
-        if args.bootdev:
-            parser.error(error_str.format(attribute="--bootdev",
-                                          action=action))
-        if args.waited_event:
-            parser.error(error_str.format(attribute="--wait-event",
-                                          action=action))
-        if args.overwrite_disks:
-            parser.error(error_str.format(attribute="--overwrite-disks",
-                                          action=action))
-        if args.recreate_networks:
-            parser.error(error_str.format(attribute="--recreate-networks",
-                                          action=action))
-        # for wait
-        if args.event:
-            parser.error(error_str.format(attribute="--event", action=action))
-
-    elif action == "wait":
-
-        # for boot
-        if args.bootdev:
-            parser.error(error_str.format(attribute="--bootdev",
-                                          action=action))
-        if args.waited_event:
-            parser.error(error_str.format(attribute="--wait-event",
-                                          action=action))
-        if args.overwrite_disks:
-            parser.error(error_str.format(attribute="--overwrite-disks",
-                                          action=action))
-        if args.recreate_networks:
-            parser.error(error_str.format(attribute="--recreate-networks",
-                                          action=action))
-        # for gen
-        if args.filename:
-            parser.error(error_str.format(attribute="--filename",
-                                          action=action))
 def check_disks(cloubed, args):
 
     if args.overwrite_disks:
 
         disks = args.overwrite_disks
-       
+
         if "yes" in disks:
 
             if len(disks)>1:
-                logging.error(u"--overwrite-disks parameter cannot contain" \
-                                  " 'yes' among other values")
-                sys.exit(1)
+                raise CloubedArgumentException(u"--overwrite-disks parameter" \
+                                  " cannot contain 'yes' among other values")
             else:
                 return True
 
         elif "no" in disks:
 
             if len(disks)>1:
-                logging.error(u"--overwrite-disks parameter cannot contain" \
-                                  " 'no' among other values")
-                sys.exit(1)
+                raise CloubedArgumentException(u"--overwrite-disks parameter" \
+                                  " cannot contain 'no' among other values")
             else:
                 return False
 
@@ -191,10 +56,10 @@ def check_disks(cloubed, args):
                                      for disk in domain.get_disks()]
             for disk in disks:
                 if disk not in domain_disks_list:
-                    logging.error(u"disk {disk} not found for domain {domain}" \
-                                      " in YAML file" \
-                                      .format(disk=disk, domain=domain_name)) 
-                    sys.exit(1)
+                    raise CloubedArgumentException(u"disk {disk} not found for"\
+                              " domain {domain} in YAML file" \
+                                                    .format(disk=disk,
+                                                            domain=domain_name))
             return disks
 
     else:
@@ -211,18 +76,18 @@ def check_networks(cloubed, args):
         if "yes" in networks:
 
             if len(networks)>1:
-                logging.error(u"--recreate-networks parameter cannot contain" \
-                                  " 'yes' among other values")
-                sys.exit(1)
+                raise CloubedArgumentException(u"--recreate-networks" \
+                                  " parameter cannot contain 'yes' among" \
+                                  " other values")
             else:
                 return True
 
         elif "no" in networks:
 
             if len(networks)>1:
-                logging.error(u"--recreate-networks parameter cannot contain" \
-                                  " 'no' among other values")
-                sys.exit(1)
+                raise CloubedArgumentException(u"--recreate-networks" \
+                                  " parameter cannot contain 'no' among" \
+                                  " other values")
             else:
                 return False
 
@@ -233,11 +98,10 @@ def check_networks(cloubed, args):
                                      for netif in domain.get_netifs()]
             for network in networks:
                 if network not in domain_networks_list:
-                    logging.error(u"network {network} not found for domain" \
-                                      " {domain} in YAML file" \
-                                      .format(network=network,
-                                              domain=domain_name))
-                    sys.exit(1)
+                    raise CloubedArgumentException(
+                              u"network {network} not found for domain {domain}" \
+                               "in YAML file".format(network=network,
+                                                     domain=domain_name))
             return networks
 
     else:
@@ -252,113 +116,96 @@ def check_event(cloubed, args):
         waited_event_str = args.waited_event[0]
         waited_event = waited_event_str.split(':')
         if len(waited_event) is not 2:
-            logging.error(u"Badly formated --wait-event parameter, should" \
-                              " respect format <event_type>:<event_detail>")
-            sys.exit(1)
+            raise CloubedArgumentException(u"Badly formated --wait-event" \
+                      " parameter, should respect format" \
+                      " <event_type>:<event_detail>")
         return waited_event
     else:
 
         logging.debug(u"--wait-event parameter not specified")
         return None
 
-def check_bootdev(args):
-
-    if args.bootdev:
-        return args.bootdev
-    else:
-        return 'hd' # default value
-        # It is not set in parameter of argparse.add_ergument(default='hd')
-        # because in this case args.bootdev is therefore defined all the times
-        # and it raises errors in check_args_coherency() for action != boot
-
 def main():
 
     """ run_cloubed: function launched by main() """
 
-    parser, args = parse_args()
-
-    # enable debug mode
-    if args.debug:
-        logging.basicConfig(format='%(levelname)-7s: %(message)s',
-                            level=logging.DEBUG)
-
-    check_args_coherency(parser, args)
+    parser = CloubedArgumentParser(u"Perform action over your virtual testbed")
+    parser.add_args()
 
     try:
-        cloubed = Cloubed()
-    except CloubedException as cdb_error:
-        # in most cases because yaml file not found
-        # TODO: manage this case specifically for a better error message
-        logging.error(u"Error while initializing cloubed: {error}" \
-                          .format(error=str(cdb_error)))
-        sys.exit(1)
+        args = parser.parse_args()
 
-    action_name = args.actions[0]
+        # enable debug mode
+        if args.debug:
+            logging.basicConfig(format='%(levelname)-7s: %(message)s',
+                                level=logging.DEBUG)
 
-    if action_name == "gen":
+        parser.check_args_coherency()
 
-         domain_name = args.domain[0]
-         filename = args.filename[0]
+        try:
+            cloubed = Cloubed()
+        except CloubedException as cdb_error:
+            # in most cases because yaml file not found
+            # TODO: manage this case specifically for a better error message
+            logging.error(u"Error while initializing cloubed: {error}" \
+                              .format(error=str(cdb_error)))
+            sys.exit(1)
 
-         logging.debug(u"Action gen on {domain} with template {template}" \
-                           .format(domain=domain_name,
-                                   template=filename))
+        action_name = args.actions[0]
 
-         try:
+        if action_name == "gen":
+
+             domain_name = args.domain[0]
+             filename = args.filename[0]
+
+             logging.debug(u"Action gen on {domain} with template {template}" \
+                               .format(domain=domain_name,
+                                       template=filename))
+
              cloubed.gen_file(domain_name, filename)
-         except CloubedException as cdb_error:
-             logging.error(cdb_error)
-         except KeyboardInterrupt:
-             logging.info(u"Cloubed stopped.")
 
-    elif action_name == u"boot":
+        elif action_name == u"boot":
 
-         domain_name = args.domain[0]
-         disks_to_overwrite = check_disks(cloubed, args)
-         networks_to_recreate = check_networks(cloubed, args)
-         waited_event = check_event(cloubed, args)
-         bootdev = check_bootdev(args)
+             domain_name = args.domain[0]
+             disks_to_overwrite = check_disks(cloubed, args)
+             networks_to_recreate = check_networks(cloubed, args)
+             waited_event = check_event(cloubed, args)
+             bootdev = check_bootdev(args)
 
-         logging.debug(u"Action boot on {domain}" \
-                           .format(domain=domain_name))
+             logging.debug(u"Action boot on {domain}" \
+                               .format(domain=domain_name))
 
-         try:
              cloubed.boot_vm(domain_name, bootdev,
                              disks_to_overwrite,
                              networks_to_recreate)
-         except CloubedException as cdb_error:
-             logging.error(cdb_error)
-             sys.exit(1)
 
-         if waited_event is not None:
-             event_type = waited_event[0]
-             event_detail = waited_event[1]
-             try:
+             if waited_event is not None:
+                 event_type = waited_event[0]
+                 event_detail = waited_event[1]
                  cloubed.wait_event(domain_name, event_type, event_detail)
-             except CloubedException as cdb_error:
-                 logging.error(cdb_error)
-             except KeyboardInterrupt:
-                 logging.info(u"Cloubed stopped.")
 
-    elif action_name == u"wait":
+        elif action_name == u"wait":
 
-         domain = args.actions[1]
-         event_type = args.actions[2]
-         event_detail = args.actions[3]
+             domain = args.actions[1]
+             event_type = args.actions[2]
+             event_detail = args.actions[3]
 
-         logging.debug(u"Action wait on {domain} with " \
-                           "{event_type}/{event_detail}" \
-                           .format(domain=domain,
-                                   event_type=event_type,
-                                   event_detail=event_detail))
-         try:
+             logging.debug(u"Action wait on {domain} with " \
+                               "{event_type}/{event_detail}" \
+                               .format(domain=domain,
+                                       event_type=event_type,
+                                       event_detail=event_detail))
              cloubed.wait_event(domain, event_type, event_detail)
-         except CloubedException as cdb_error:
-             logging.error(cdb_error)
-         except KeyboardInterrupt:
-             logging.info(u"Cloubed stopped.")
 
-    else:
+        else:
+            raise CloubedArgumentException(
+                      u"Unknown action '{action}'".format(action=action_name))
 
-        logging.error(u"Unknown action '{action}'".format(action=action_name))
+    except CloubedArgumentException as cdb_error:
+        logging.error(cdb_error)
         sys.exit(1)
+    except CloubedException as cdb_error:
+        logging.error(cdb_error)
+        sys.exit(1)
+    except KeyboardInterrupt:
+        logging.info(u"Cloubed stopped.")
