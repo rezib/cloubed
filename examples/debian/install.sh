8d7e9ddda4c7e2625018680bf1e1f33eb18ea9c1
Rémi Palancher <remi@rezib.org>
2013-09-17 22:01:13 +0200

    adapt debian example shell script for wait event
    The shell script that gives an example of cloubed CLI script usage for
    installing a minimal Debian VM has been adapted for using new working wait
    action. Also avoid using removed argument --wait-event with boot action.

--- examples/debian/install.sh
+++ examples/debian/install.sh
@@ -8,7 +8,9 @@ VM="vmtest"
 echo "generating template files"
 cloubed $OPTS gen --domain=$VM --filename=ipxe
 cloubed $OPTS gen --domain=$VM --filename=preseed
-echo "booting VM $VM and waiting for shutdown"
-cloubed $OPTS boot --domain=$VM --bootdev=network --overwrite-disks=yes --recreate-networks=yes --wait-event=stopped:shutdown
+echo "booting VM $VM"
+cloubed $OPTS boot --domain=$VM --bootdev=network --overwrite-disks=yes --recreate-networks=yes
+echo "waiting VM $VM for shutdown"
+cloubed $OPTS wait --domain=$VM --event=stopped:shutdown
 echo "re-booting VM $VM"
 cloubed $OPTS boot --domain=$VM
