ff5a85ecf0d9ffe02a76d655d5b77359e3b5fdf9
Rémi Palancher <remi@rezib.org>
2013-09-24 21:19:56 +0200

    only add host_ip in list if not None
    Previously, if Network.get_ip_host() returned None, None was added in the list
    list_ip_hosts built with list comprehension. It is now fixed with added if.

--- lib/Cloubed.py
+++ lib/Cloubed.py
@@ -207,7 +207,9 @@ class Cloubed():
             # build the list of host ip addresses on all networks connected to
             # the domain
             list_ip_hosts = [ dom_netif.get_network().get_ip_host() \
-                              for dom_netif in domain.get_netifs() ]
+                              for dom_netif in domain.get_netifs() \
+                              if dom_netif.get_network().get_ip_host() \
+                                 is not None ]
             if len(list_ip_hosts) > 0:
                 # arbitrary take the first ip address
                 address = list_ip_hosts[0]
