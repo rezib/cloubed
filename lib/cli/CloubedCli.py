rezib/cloubed

commit bbbdd6b5928c35bc2d2051035ecfbe57d769184d

move cli check_*() args checking functions
Functions check_disks(), check_networks() and check_event() of module
CloubedCli are now methods of CloubedArgumentParser class. At the same
time, their names have changed from check_*() to parse_*() for
homogeneity with other method of this class.

The logic to check the values of these parameters against the content of
cloubed YAML file has been removed because:
1/ it didn't work.
2/ it shouldn't be better be done in lib/Domain.py. TODOs have been
   added at appropriate lines of this line for future patches.

lib/cli/CloubedCli.py
@@ -27,104 +27,6 @@ from ..cli.CloubedArgumentParser import CloubedArgumentParser
 import sys
 import logging
 
-def check_disks(cloubed, args):
-
-    if args.overwrite_disks:
-
-        disks = args.overwrite_disks
-
-        if "yes" in disks:
-
-            if len(disks)>1:
-                raise CloubedArgumentException(u"--overwrite-disks parameter" \
-                                  " cannot contain 'yes' among other values")
-            else:
-                return True
-
-        elif "no" in disks:
-
-            if len(disks)>1:
-                raise CloubedArgumentException(u"--overwrite-disks parameter" \
-                                  " cannot contain 'no' among other values")
-            else:
-                return False
-
-        else:
-
-            # check if all disks are declared in YAML file for this domain
-            domain_disks_list = [disk.get_storage_volume().get_name() \
-                                     for disk in domain.get_disks()]
-            for disk in disks:
-                if disk not in domain_disks_list:
-                    raise CloubedArgumentException(u"disk {disk} not found for"\
-                              " domain {domain} in YAML file" \
-                                                    .format(disk=disk,
-                                                            domain=domain_name))
-            return disks
-
-    else:
-
-        logging.debug(u"--overwrite-disks not defined, defaulting to 'no'")
-        return False
-
-def check_networks(cloubed, args):
-
-    if args.recreate_networks:
-
-        networks = args.recreate_networks
-
-        if "yes" in networks:
-
-            if len(networks)>1:
-                raise CloubedArgumentException(u"--recreate-networks" \
-                                  " parameter cannot contain 'yes' among" \
-                                  " other values")
-            else:
-                return True
-
-        elif "no" in networks:
-
-            if len(networks)>1:
-                raise CloubedArgumentException(u"--recreate-networks" \
-                                  " parameter cannot contain 'no' among" \
-                                  " other values")
-            else:
-                return False
-
-        else:
-
-            # check if all networks are declared in YAML file for this domain
-            domain_networks_list = [netif.get_network().get_name() \
-                                     for netif in domain.get_netifs()]
-            for network in networks:
-                if network not in domain_networks_list:
-                    raise CloubedArgumentException(
-                              u"network {network} not found for domain {domain}" \
-                               "in YAML file".format(network=network,
-                                                     domain=domain_name))
-            return networks
-
-    else:
-
-        logging.debug(u"--recreate-networks not defined, defaulting to 'no'")
-        return False
-
-def check_event(cloubed, args):
-
-    if args.event:
-
-        waited_event_str = args.event[0]
-        waited_event = waited_event_str.split(':')
-        if len(waited_event) is not 2:
-            raise CloubedArgumentException(u"Badly formated --event" \
-                      " parameter, should respect format" \
-                      " <event_type>:<event_detail>")
-        return waited_event
-    else:
-
-        logging.debug(u"--event parameter not specified")
-        return None
-
 def print_testbed_infos(testbed):
      """
          Prints nicely a dict full of informations about the testbed and its
@@ -235,9 +137,9 @@ def main():
         elif action_name == u"boot":
 
              domain_name = args.domain[0]
-             disks_to_overwrite = check_disks(cloubed, args)
-             networks_to_recreate = check_networks(cloubed, args)
-             bootdev = parser.check_bootdev()
+             disks_to_overwrite = parser.parse_disks(cloubed)
+             networks_to_recreate = parser.parse_networks(cloubed)
+             bootdev = parser.parse_bootdev()
 
              logging.debug(u"Action boot on {domain}" \
                                .format(domain=domain_name))
@@ -249,7 +151,7 @@ def main():
         elif action_name == u"wait":
 
              domain_name = args.domain[0]
-             waited_event = check_event(cloubed, args)
+             waited_event = parser.parse_event()
              event_type = waited_event[0]
              event_detail = waited_event[1]
 

lib/Domain.py
@@ -325,6 +325,7 @@ class Domain:
                 else:
                     overwrite_storage_volume = False
                 storage_volume.create(overwrite_storage_volume)
+        # TODO: check if overwrite_disks has disks not in self._disks
 
         for netif in self._netifs:
             network = netif.get_network()
@@ -334,6 +335,7 @@ class Domain:
                 else:
                     recreate_network = False
                 network.create(recreate_network)
+        # TODO: check if recreate_networks has disks not in self._netifs
 
         if overwrite:
             # delete all existing domain

lib/cli/CloubedArgumentParser.py
@@ -182,7 +182,7 @@ class CloubedArgumentParser(argparse.ArgumentParser):
                               error_str.format(attribute=arg,
                                                action=action))
 
-    def check_bootdev(self):
+    def parse_bootdev(self):
 
         if self._args.bootdev:
             return self._args.bootdev
@@ -192,6 +192,98 @@ class CloubedArgumentParser(argparse.ArgumentParser):
             # because in this case args.bootdev is therefore defined all the
             # times and it raises errors in check_args_coherency() for
             # action != boot
+
+    def parse_disks(self):
+        """
+           Parses and returns values of --overwrite-disks parameter of boot
+           action or raises exception if problem is found
+        """
+
+        if self._args.overwrite_disks:
+
+            disks = self._args.overwrite_disks
+
+            if "yes" in disks:
+
+                if len(disks)>1:
+                    raise CloubedArgumentException(u"--overwrite-disks parameter" \
+                                      " cannot contain 'yes' among other values")
+                else:
+                    return True
+
+            elif "no" in disks:
+
+                if len(disks)>1:
+                    raise CloubedArgumentException(u"--overwrite-disks parameter" \
+                                      " cannot contain 'no' among other values")
+                else:
+                    return False
+
+            else:
+
+                return disks
+
+        else:
+
+            logging.debug(u"--overwrite-disks not defined, defaulting to 'no'")
+            return False
+
+    def parse_networks(self):
+        """
+           Parses and returns values of --recreate-networks parameter of boot
+           action or raises exception if problem is found
+        """
+
+        if self._args.recreate_networks:
+
+            networks = self._args.recreate_networks
+
+            if "yes" in networks:
+
+                if len(networks)>1:
+                    raise CloubedArgumentException(u"--recreate-networks" \
+                                      " parameter cannot contain 'yes' among" \
+                                      " other values")
+                else:
+                    return True
+
+            elif "no" in networks:
+
+                if len(networks)>1:
+                    raise CloubedArgumentException(u"--recreate-networks" \
+                                      " parameter cannot contain 'no' among" \
+                                      " other values")
+                else:
+                    return False
+
+            else:
+
+                return networks
+
+        else:
+
+            logging.debug(u"--recreate-networks not defined, defaulting to 'no'")
+            return False
+
+    def parse_event(self):
+        """
+           Parses and returns values of --event parameter of wait action or
+           raises exception if problem is found
+        """
+
+        if self._args.event:
+
+            waited_event_str = self._args.event[0]
+            waited_event = waited_event_str.split(':')
+            if len(waited_event) is not 2:
+                raise CloubedArgumentException(u"Badly formated --event" \
+                          " parameter, should respect format" \
+                          " <event_type>:<event_detail>")
+            return waited_event
+        else:
+
+            logging.debug(u"--event parameter not specified")
+            return None
 
     def parse_resource(self):
 

commit 1f9fea526567b19afd82ab92cb3fbf1964102b6a

fix parse_*() do not take argument
Bug recently introduced with commit bbbdd6b592.

lib/cli/CloubedCli.py
@@ -137,8 +137,8 @@ def main():
         elif action_name == u"boot":
 
              domain_name = args.domain[0]
-             disks_to_overwrite = parser.parse_disks(cloubed)
-             networks_to_recreate = parser.parse_networks(cloubed)
+             disks_to_overwrite = parser.parse_disks()
+             networks_to_recreate = parser.parse_networks()
              bootdev = parser.parse_bootdev()
 
              logging.debug(u"Action boot on {domain}" \