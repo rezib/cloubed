74f1c6ccc18f70d8bd3238d1fdc328fb56db0831
Rémi Palancher <remi@rezib.org>
2015-04-25 13:36:04 +0200

    Update examples with 0.5 changes

--- examples/debian/install.py
+++ examples/debian/install.py
@@ -10,17 +10,17 @@ logging.basicConfig(format='%(levelname)-7s: %(message)s',
 
 try:
     print("generating template file")
-    cloubed.gen_file(domain_name = VM, template_name = "ipxe")
-    cloubed.gen_file(domain_name = VM, template_name = "preseed")
+    cloubed.gen(domain=VM, template="ipxe")
+    cloubed.gen(domain=VM, template="preseed")
     print("booting vm {vm}".format(vm=VM))
-    cloubed.boot_vm(domain_name = VM,
-                    bootdev = "network",
-                    overwrite_disks = True,
-                    recreate_networks = True )
+    cloubed.boot(domain=VM,
+                 bootdev="network",
+                 overwrite_disks=True,
+                 recreate_networks=True )
     print("waiting event shutdown on vm {vm}".format(vm=VM))
-    cloubed.wait_event(VM, "STOPPED", "SHUTDOWN")
+    cloubed.wait(VM, "STOPPED", "SHUTDOWN")
     print("booting vm {vm}".format(vm=VM))
-    cloubed.boot_vm(VM)
+    cloubed.boot(VM)
 
 except cloubed.CloubedException as cdb_error:
     print(cdb_error)
