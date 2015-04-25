491f72592d37714c1f9ba8f785dcdc82801728be
Rémi Palancher <remi@rezib.org>
2015-04-25 13:46:35 +0200

    Update copyright dates

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Cloubed'
-copyright = u'2013-2014, Rémi Palancher'
+copyright = u'2013-2015, Rémi Palancher'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
